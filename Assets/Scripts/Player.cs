61cb0e8712b3f769d01e72843907e3e100dc326b
ainetta12 <ainasanzg@gmail.com>
2023-09-20 10:15:03 +0200

    jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,27 +5,53 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]private float _playerSpeed = 5;
+
+    [SerializeField]private float _jumpForce = 5;    
+
     private float _playerInputHorizontal; 
     private float _playerInputVertical;
+
+    private Rigidbody2D _rBody2D;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rBody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        _playerInputHorizontal = Input.GetAxis("Horizontal");
-        _playerInputVertical = Input.GetAxis("Vertical");
-        
-        transform.Translate(new Vector2(_playerInputHorizontal, _playerInputVertical) *_playerSpeed * Time.deltaTime);
+      PlayerMovement();
 
+      if(Input.GetButtonDown("Jump"))
+      {
+        Jump();
+      }
     }
     
+
+    void FixedUpdate()
+    {
+       // _rBody2D.AddForce(new Vector2(_playerInputHorizontal * _playerSpeed, 0), ForceMode2D.Impulse);
+
+       _rBody2D.velocity = new Vector2(_playerInputHorizontal * _playerSpeed, _rBody2D.velocity.y);
+    }
+
+
     void PlayerMovement()
     {
+          _playerInputHorizontal = Input.GetAxis("Horizontal");
+       /* _playerInputVertical = Input.GetAxis("Vertical");
+        
+        transform.Translate(new Vector2(_playerInputHorizontal, _playerInputVertical) *_playerSpeed * Time.deltaTime);*/
+
+    }
+
+    void Jump()
+    {
+       _rBody2D.AddForce(new Vector2(0, _jumpForce), ForceMode2D.Impulse);
+
 
     }
 
